fabce11188bcf64e28b439f64bad91a91d7f0aa9
Anna Ridenour <anna.ridenour@sprinklr.com>
2024-04-02 09:19:59 -0700

    update feather sdk env name

--- src/components/RSVP.tsx
+++ src/components/RSVP.tsx
@@ -1,12 +1,12 @@
 import { init, Form } from "@feathery/react";
 import React from "react";
 
-function RSVP() {
-  const sdk = process.env.FEATHERY_SDK;
+const RSVP = () => {
+  const sdk = process.env.REACT_APP_FEATHERY_TOKEN;
   console.log("AMR", sdk);
   init(`${sdk}`);
 
   return <Form formName="Ridenour-Forst Wedding RSVP" />;
-}
+};
 
 export default RSVP;
