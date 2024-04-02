2009f8b4acf0806c291e0626a77fea72a9cd18b5
Anna Ridenour <anna.ridenour@sprinklr.com>
2024-04-02 09:06:39 -0700

    add logging to troubleshoot SDK

--- src/components/RSVP.tsx
+++ src/components/RSVP.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 function RSVP() {
   const sdk = process.env.FEATHERY_SDK;
+  console.log("AMR", sdk);
   init(`${sdk}`);
 
   return <Form formName="Ridenour-Forst Wedding RSVP" />;
